b3f0c50813f2a3da0dd2df076cf372314603024a
Stacey Turner <112356048+Arigith@users.noreply.github.com>
2022-12-17 11:20:18 +1300

    updated

--- mainfolder/main.py
+++ mainfolder/main.py
@@ -2,8 +2,26 @@ from fastapi import FastAPI
 from database import engine
 from routers import journal, user, authentication
 import models
+# added import so Astro will work
+from fastapi.middleware.cors import CORSMiddleware
+# end addition
 
 app=FastAPI()
+
+# added for Astro
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# end additions
 
 models.Base.metadata.create_all(engine)
 
